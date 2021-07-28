df0066e44e1f7a4089a1c904f83cdc45279c1165
Caio Sampaio <caio.sampaio.dev@gmail.com>
2021-07-28 16:08:12 -0300

    create queue

---
+++ src/components/CreateQueueModal/CreateQueueModal.jsx
@@ -0,0 +1,33 @@
+import {
+  BdsAlert,
+  BdsAlertHeader,
+  BdsAlertBody,
+  BdsAlertActions,
+  BdsButton,
+  BdsInput,
+  BdsTypo,
+} from "blip-ds/dist/blip-ds-react";
+import React, { useRef } from "react";
+import "./createQueueModal.css";
+
+export const CreateQueueModal = ({ open = false, handleClick = (isSave, value) => {} }) => {
+  const inputRef = useRef(null);
+  return (
+    <BdsAlert open={open}>
+      <BdsAlertHeader icon="info" variant="system">
+        {"Criação de nova fila de atendimento"}
+      </BdsAlertHeader>
+      <BdsAlertBody>
+        <BdsInput ref={inputRef} label="Nome da nova fila" />
+      </BdsAlertBody>
+      <BdsAlertActions>
+        <BdsButton variant="primary" onClick={() => handleClick(true, inputRef.current.value)}>
+          {"Salvar"}
+        </BdsButton>
+        <BdsButton  variant="secondary" onClick={() => handleClick(false)}>
+          {"Cancelar"}
+        </BdsButton>
+      </BdsAlertActions>
+    </BdsAlert>
+  );
+};
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,28 +1,74 @@
 import blipServices from "api/blipServices";
-import { withLoading } from "api/commonServices";
+import { showToast, withLoading, withoutLoading } from "api/commonServices";
 import helperServices from "api/helpersServices";
-import iframeService from "api/iframeServices";
+import iframeServices from "api/iframeServices";
 import { BdsButton, BdsTypo } from "blip-ds/dist/blip-ds-react";
+import { CreateQueueModal } from "components/CreateQueueModal/CreateQueueModal";
 import { QueueList } from "components/QueueList/QueueList";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const HomePage = () => {
   const [queues, setQueues] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     withLoading(async () => {
-      const queuesFromDesk = await iframeService.getQueues();
-      if (queuesFromDesk.length > 0) {
-        const sorted = helperServices.sortQueues(queuesFromDesk);
-        setQueues(sorted);
-      }
+      await setQueuesListData();
     });
   }, []);
+
+  useEffect(() => {
+    if(!isModalOpen) {
+      withoutLoading(async () => {
+        await setQueuesListData();
+      });
+    }
+  }, [isModalOpen])
+
+  console.log('queues :>> ', queues);
+
+  const setQueuesListData = async () => {
+    debugger
+    const queuesFromDesk = await iframeServices.getQueues();
+    if (queuesFromDesk.length > 0) {
+      const sorted = helperServices.sortQueues(queuesFromDesk);
+      setQueues(sorted);
+    }
+  };
 
   const handleSwitchQueue = async (e, queue) => {
     const checked = e.target.checked;
     await blipServices.changeQueueStatus(checked, queue);
+  };
+
+  const handleCreateQueueClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClick = async (isSave, value) => {
+    try {
+      if (!isSave) {
+        setIsModalOpen(false);
+      }
+  
+      const response = await blipServices.setQueue({
+        name: value,
+        isActive: true
+      })
+      const success = response !== null;
+
+      showToast({
+        title: success ? null : "Algo deu errado...",
+        type: success ? "success" : "danger",
+        message: success
+          ? "Fila criada com sucesso!"
+          : "Houve um erro ao salvar a fila, tente novamente.",
+      });
+      
+    } finally {
+      setIsModalOpen(false);
+    }   
   };
 
   return (
@@ -31,9 +77,9 @@ export const HomePage = () => {
         <BdsTypo variant="fs-20" bold="bold" className="hydrated">
           {"Filas de atendimento"}
         </BdsTypo>
-        {/* <Link to={`/createQueue`}>
-          <BdsButton icon="add">{"Criar nova fila"}</BdsButton>
-        </Link> */}
+        <BdsButton icon="add" onClick={handleCreateQueueClick}>
+          {"Criar nova fila"}
+        </BdsButton>
       </div>
       {queues.length > 0 ? (
         <QueueList queues={queues} handleSwitchQueue={handleSwitchQueue} />
@@ -42,6 +88,7 @@ export const HomePage = () => {
           {"Você ainda não possui nenhuma fila de atendimento cadastrada."}
         </BdsTypo>
       )}
+      <CreateQueueModal open={isModalOpen} handleClick={handleModalClick} />
     </div>
   );
 };
